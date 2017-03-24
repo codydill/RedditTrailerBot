0d670aa03c441c24f9f4a364a55943aa1d07fd02
Cody Dillon <codywdillongithub@gmail.com>
2017-03-24 00:40:48 -0700

    Added functions for validating domain and checking for existing trailer post.

--- TrailerBot.js
+++ TrailerBot.js
@@ -15,4 +15,36 @@ const netflix_sub_name = 'NetflixBestOf';
 const netflix_domain_name = 'netflix.com';
 
 var r = new snoowrap(CONFIG);
-r.getSubreddit(netflix_sub_name).getNew().map(post => post.title).then(console.log);
+var listing = r.getSubreddit(netflix_sub_name).getNew({limit : 10}).then(all => {
+    all.forEach(post => {
+        if (isValidDomain(post)) {
+            post.expandReplies().then(printValidPost);
+        }
+    })
+});
+
+function parseTitle(title) {
+
+}
+
+function isValidDomain(post) {
+    return post.domain === netflix_domain_name;
+}
+
+function hasTrailer(post) {
+    for (let comment of post.comments){
+        if (comment.author.name === CONFIG.username) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function printValidPost(post){
+    if (isValidDomain(post) && !hasTrailer(post)) {
+        console.log(post.title + " " + post.id);
+        for (let comment of post.comments) {
+            console.log(comment.author.name);
+        }
+    }
+}
